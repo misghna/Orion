478b5dbb0bc13ece5d554252ec9a809a9bcd961b
Msghe <sessnu@gmail.com>
2016-08-26 13:28:21 -0600

    server checkin

---
+++ Orion/orion-server/src/main/java/com/sesnu/orion/config/SpringWebConfig.java
@@ -0,0 +1,34 @@
+package com.sesnu.orion.config;
+ 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+ 
+@EnableWebMvc
+@Configuration
+@ComponentScan({ "com.sesnu.orion.web" })
+public class SpringWebConfig extends WebMvcConfigurerAdapter {
+ 
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	//	registry.addResourceHandler("/resources/prod/assets/**").addResourceLocations("/assets/");
+		registry.addResourceHandler("/assets/**").addResourceLocations("/resources/assets/");
+		registry.addResourceHandler("/css/**").addResourceLocations("/resources/css/");
+		registry.addResourceHandler("/js/**").addResourceLocations("/resources/js/");
+	}
+	
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/views/jsp/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+ 
+}
---
+++ Orion/orion-server/src/main/java/com/sesnu/orion/web/jsonview/Views.java
@@ -0,0 +1,5 @@
+package com.sesnu.orion.web.jsonview;
+
+public class Views {
+    public static class Public {}
+}
